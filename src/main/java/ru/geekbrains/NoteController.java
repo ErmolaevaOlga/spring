a7633477b0c8fd0855b86084a984dc650e7fd544
Ольга Ермолаева <ermooolga@gmail.com>
2024-04-30 12:40:40 +0700

    add proect

---
+++ src/main/java/ru/geekbrains/NoteController.java
@@ -0,0 +1,28 @@
+package ru.geekbrains;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import com.example.demo.model.Note;
+import com.example.demo.services.FileGateway;
+
+import java.time.LocalDateTime;
+
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/notes")
+public class NoteController {
+    private final FileGateway fileGateway;
+
+
+
+    @PostMapping
+    public ResponseEntity<Note> addNote(@RequestBody Note note) {
+        note.setCreation(LocalDateTime.now());
+        fileGateway.writeToFile(note.getTitle() + ".txt", note.toString());
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+}
---
+++ src/test/java/com/example/Demo.java
@@ -0,0 +1,9 @@
+package com.example;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+@SpringBootTest
+public class Demo {
+    @Test
+    void contextLoads() {
+    }
+}
